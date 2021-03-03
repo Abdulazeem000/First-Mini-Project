977302975a5c57d5ee63323091607958d74494e2
Muhammad Azeem <azeemarain000@gmaol.com>
2021-03-03 19:24:16 +0500

    First Mini Project

---
+++ resources/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {  
+
+
+$('.js--wp-2').waypoint(function(direction){
+          
+          
+          $('.js--wp-2').addClass('animated fadeInUp');
+                              
+                          
+     },{
+          
+          
+       offset : '50%'    
+      
+      });
+
+
+});
